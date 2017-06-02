bb9ea08952983441dff159f45f56d0cb109487aa
grahamlyons <graham@grahamlyons.com>
2017-06-02 14:55:12 +0100

    Don't allocate a tty if none is available

--- test.sh
+++ test.sh
@@ -6,7 +6,8 @@ image_id=cdflow-wrapper.test
 
 docker build -t "${image_id}" .
 
-docker run --rm -it --name "${image_id}" "${image_id}" \
+docker run --rm --name "${image_id}" "${image_id}" \
+    -i $(tty -s && echo -t) \
     py.test \
     --cov=. \
     --cov-report term-missing \
