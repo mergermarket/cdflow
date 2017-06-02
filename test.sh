4b3fb0ed368ad06a16c57b824ac32517c4294bf1
grahamlyons <graham@grahamlyons.com>
2017-06-02 14:56:30 +0100

    Reorder the arguments so flags are not treated as image

--- test.sh
+++ test.sh
@@ -6,8 +6,9 @@ image_id=cdflow-wrapper.test
 
 docker build -t "${image_id}" .
 
-docker run --rm --name "${image_id}" "${image_id}" \
+docker run --rm --name "${image_id}" \
     -i $(tty -s && echo -t) \
+    "${image_id}" \
     py.test \
     --cov=. \
     --cov-report term-missing \
