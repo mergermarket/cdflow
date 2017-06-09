c1faf0bc33f3d2496143428e3f465b2de9ff0229
grahamlyons <graham@grahamlyons.com>
2017-06-09 18:59:10 +0100

    Refactor - get release bucket from account scheme
    We have an account scheme in an S3 bucket accessible to all relevant
    credentials. We fetch the account scheme from there, rather than
    assume a role and finding the bucket, and the releases bucket is
    listed.

--- test/test_config.py
+++ test/test_config.py
@@ -1,75 +1,13 @@
 import unittest
-from string import digits, printable
+from string import printable
 
-from mock import patch, MagicMock, Mock
+from mock import patch, Mock
 from hypothesis import given, assume
 from hypothesis.strategies import dictionaries, fixed_dictionaries, text
 
-import yaml
+from strategies import image_id, VALID_ALPHABET
 
-from strategies import image_id
-
-from cdflow import get_account_id, get_image_id, assume_role, CDFLOW_IMAGE_ID
-
-
-class TestAssumeRole(unittest.TestCase):
-
-    @patch('cdflow.Session')
-    def test_assume_role_in_account(self, Session):
-        root_session = Mock(region_name='eu-north-1')
-        sts_client = Mock()
-        sts_client.assume_role.return_value = {
-            'Credentials': {
-                'AccessKeyId': 'foo',
-                'SecretAccessKey': 'bar',
-                'SessionToken': 'baz',
-            }
-        }
-        root_session.client.return_value = sts_client
-
-        account_id = '123456789'
-        session_name = 'foo@bar.com'
-
-        session = assume_role(root_session, account_id, session_name)
-
-        assert session == Session.return_value
-
-        sts_client.assume_role.assert_called_once_with(
-            RoleArn='arn:aws:iam::{}:role/admin'.format(account_id),
-            RoleSessionName=session_name,
-        )
-
-        Session.assert_called_once_with(
-            'foo', 'bar', 'baz', root_session.region_name
-        )
-
-
-class TestGetAccountId(unittest.TestCase):
-
-    @given(text(alphabet=digits))
-    def test_get_account_id(self, account_id):
-        with patch('cdflow.open') as open_, \
-                patch('cdflow.BytesIO') as BytesIO:
-
-            config_file = MagicMock(spec=file)
-            config_file.read.return_value = yaml.dump({
-                'account_prefix': 'foo',
-            })
-            open_.return_value.__enter__.return_value = config_file
-
-            s3_bucket = Mock()
-
-            mock_file = Mock()
-            mock_file.read.return_value = account_id
-
-            BytesIO.return_value.__enter__.return_value = mock_file
-
-            assert account_id == get_account_id(s3_bucket)
-            mock_file.seek.assert_called_once_with(0)
-            s3_bucket.download_fileobj.assert_called_once_with(
-                'foodev',
-                mock_file,
-            )
+from cdflow import get_image_id, fetch_account_scheme, CDFLOW_IMAGE_ID
 
 
 class TestGetReleaseCommandsImage(unittest.TestCase):
@@ -97,3 +35,53 @@ class TestGetReleaseCommandsImage(unittest.TestCase):
         image_id = get_image_id(environment)
 
         assert image_id == fixtures['image_id']
+
+
+class TestFetchAccountScheme(unittest.TestCase):
+
+    @given(text(alphabet=VALID_ALPHABET, min_size=1))
+    def test_fetch_account_scheme(self, account_prefix):
+        s3_resource = Mock()
+
+        with patch('cdflow.BytesIO') as BytesIO:
+            BytesIO.return_value.__enter__.return_value.read.return_value = '''
+                {{
+                  "accounts": {{
+                    "{0}dev": {{
+                      "id": "222222222222",
+                      "role": "admin"
+                    }},
+                    "{0}prod": {{
+                      "id": "111111111111",
+                      "role": "admin"
+                    }}
+                  }},
+                  "release-account": "{0}dev",
+                  "release-bucket": "{0}-account-resources",
+                  "environments": {{
+                    "live": "{0}prod",
+                    "*": "{0}dev"
+                  }},
+                  "default-region": "eu-west-12",
+                  "ecr-registry": "1234567.dkr.ecr.eu-west-1.amazonaws.com",
+                  "lambda-bucket": "cdflow-lambda-releases"
+                }}
+            '''.format(account_prefix)
+
+            account_scheme = fetch_account_scheme(s3_resource, account_prefix)
+
+        expected_keys = sorted([
+            'accounts', 'release-account', 'release-bucket', 'environments',
+            'default-region', 'ecr-registry', 'lambda-bucket'
+        ])
+
+        assert map(str, sorted(account_scheme.keys())) == expected_keys
+
+        release_bucket = '{}-account-resources'.format(account_prefix)
+
+        assert account_scheme['release-bucket'] == release_bucket
+
+        s3_resource.Object.assert_called_once_with(
+            '{}-account-resources'.format(account_prefix),
+            'account-scheme.json'
+        )
