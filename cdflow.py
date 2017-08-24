a54ea5efbaabab6d9c045919f657c3e78e2d2d8e
Neil Haggerty <neil.haggerty@mergermarket.com>
2017-08-24 12:22:46 +0100

    Changes encoding message so it works with python3

--- cdflow.py
+++ cdflow.py
@@ -194,7 +194,7 @@ def _print_logs(container):
     for message in container.logs(
         stream=True, follow=True, stdout=True, stderr=True
     ):
-        print(message, end='')
+        print(message.decode(), end='')
 
 
 def _remove_container(container):
