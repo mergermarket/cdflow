8cfc89104fbbfb96fd2ec7730a189a311e0694c6
Keir Badger <keir.badger@acuris.com>
2018-01-31 16:48:37 +0000

    Jenkins Slave Pool
        Changed the delimiter of the release data to be an equals sign
        i.e. --release-data key=value

--- test/test_integration.py
+++ test/test_integration.py
@@ -19,7 +19,7 @@ class TestIntegration(unittest.TestCase):
     @given(filepath())
     def test_release(self, project_root):
         argv = ['release', '--platform-config', '../path/to/config',
-                '--release-data ami_id:ami-z9876', '42']
+                '--release-data ami_id=ami-z9876', '42']
         with patch('cdflow.docker') as docker, \
                 patch('cdflow.os') as os:
 
