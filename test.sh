6cd970af903a9b03f802ef50f6f75d60467c08bd
grahamlyons <graham@grahamlyons.com>
2018-02-01 11:34:00 +0000

    Take out lint-only mode
    Fails with 'unbound variable' when $1 is not set, due to -u flag.
    Can't work out how to fix it right now so take it out.

--- test.sh
+++ test.sh
@@ -6,16 +6,14 @@ image_id="$(basename $(pwd))"
 
 docker build -t "${image_id}" .
 
-if [ ${1} != 'lint-only' ]
-then
-    docker run --rm --name "${image_id}" \
-        -i $(tty -s && echo -t) \
-        "${image_id}" \
-        py.test \
-        --cov=. \
-        --cov-report term-missing \
-        "${@}"
-fi
+docker run --rm --name "${image_id}" \
+    -i $(tty -s && echo -t) \
+    "${image_id}" \
+    py.test \
+    --cov=. \
+    --cov-report term-missing \
+    "${@}"
+
 
 docker run --rm --name "${image_id}" "${image_id}" \
     flake8 --max-complexity=5
