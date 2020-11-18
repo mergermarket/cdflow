0101c8ab1292be8e90bd0432b2317f32843457c6
Gary Richards <gary.richards@acuris.com>
2020-11-18 11:49:24 +0000

    Correct the acceptance tests
    From a second build on the same Docker host, if the Dockerfile does not
    change, the layers created by previous builds are cached and reused,
    even RUN instructions.
    
    Forcefully invalidate that cache just before the RUN instruction that
    does the real work to ensure that cdflow in the resultant container is
    always the latest from the git repository checked out, even if it's from
    the master branch

--- acceptance-tests/test.sh
+++ acceptance-tests/test.sh
@@ -7,6 +7,7 @@ AC_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker image build -t "${IMAGE_ID}" \
     --build-arg CDFLOW_COMMIT_SHA=${CDFLOW_COMMIT_SHA:-master} \
+    --build-arg ALWAYS_INVALIDATE_BUILD_CACHE_HERE=$(date +%Y%m%d%H%M%S) \
     $AC_FOLDER
 
 root=$(git rev-parse --show-toplevel)
